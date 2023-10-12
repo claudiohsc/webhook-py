c8b7644cb59bcbdcdf4ff2715bba85c1735e4f11
CLaudio Henrique <claudio885henrique@gmail.com>
2023-10-12 18:45:41 -0300

    rota clientes

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+clientes = {
+    1: {"nome": "joao", "telefone": 5599999999999, "status": "emitido"},
+    2: {"nome": "maria", "telefone": 5599999999999, "status": "emitido"},
+    3: {"nome": "lisa", "telefone": 5599999999999, "status": "pago"},
+    4: {"nome": "mario", "telefone": 5599999999999, "status": "emitido"}    
+}
+
+@app.get("/")
+def home():
+    return {"Clientes": len(clientes)}
+
+
+@app.get("/clientes/{id_cliente}")
+def get_cliente(id_cliente: int):
+    return clientes[id_cliente]
